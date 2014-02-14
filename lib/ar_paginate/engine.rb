4a5a0ad244bc0add12c8b2114d3f5942ea7bee8c
Revath S Kumar <rsk@revathskumar.com>
2014-02-14 18:54:51 +0530

    remove print statement

--- lib/ar_paginate/engine.rb
+++ lib/ar_paginate/engine.rb
@@ -2,7 +2,6 @@ module ArPaginate
   class Engine < ::Rails::Engine
     initializer 'ar_paginate.setup' do |app|
       app.config.assets.paths << File.join(config.root, 'vendor')
-      p app.config.assets.paths
     end
   end
 end
